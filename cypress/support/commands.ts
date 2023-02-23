f285259646f1addf7b0d3e67470f65e69380355b
Shea McKinney <sheamck@stanford.edu>
2023-02-22 21:53:41 -0800

    ADAPTHR-1: Setup Repo.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,7 @@
 // in cypress/support/index.ts
 // load type definitions that come with Cypress module
 /// <reference types="cypress" />
-import { mockData } from '../../src/services/megaprofile/mocks/mock-data/mock-data';
-import { maxDataUser } from '../../src/services/megaprofile/mocks/mock-data/constituents';
-
+import { mockData, maxDataUser } from './mockUsers';
 // Now we're a module
 export {};
 
--- cypress/support/mockUsers.ts
+++ cypress/support/mockUsers.ts
@@ -1,4 +1,42 @@
 // TODO: Replace mock data from mock server with our own static mocks.
-const mockData = { constituents: {} };
-export const maxDataUser = mockData.constituents['1234567890'];
-export const minDataUser = mockData.constituents['1000000000'];
+export const maxDataUser = {
+  encodedSUID: 1234567890,
+  contact: {
+    name: {
+      username: 'maxdatauser',
+      fullNameParsed: {
+        firstName: 'Max',
+        lastName: 'Data',
+      },
+    },
+  },
+  emails: {
+    id: '21D2DEF98D53401F81B1CFC786495F3CC4E996A6EDFD312EDC5658260046C53B',
+    emailAddress: 'example@example.com',
+    type: 'Business Email',
+  },
+};
+export const minDataUser = {
+  encodedSUID: 1000000000,
+  contact: {
+    name: {
+      username: 'mindatauser',
+      fullNameParsed: {
+        firstName: 'Min',
+        lastName: 'Data',
+      },
+    },
+  },
+  emails: {
+    id: '21D2DEF98D53401F81B1CFC786495F3CC4E996A6EDFD312EDC5658260046C53B',
+    emailAddress: 'example@example.com',
+    type: 'Home Email',
+  },
+};
+
+export const mockData = {
+  constituents: {
+    minDataUser,
+    maxDataUser,
+  },
+};
